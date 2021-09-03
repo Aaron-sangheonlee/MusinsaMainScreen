4e29d6b212cea10127523dc6ec7d49930d6e9aaf
Aaron-sangheonlee <jas2sh@naver.com>
2021-09-03 16:16:38 +0900

    create musinsa mainscreen

---
+++ MUSINSA mainScreen/MUSINSA mainScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MUSINSA mainScreen
+//
+//  Created by 이상헌 on 2021/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
